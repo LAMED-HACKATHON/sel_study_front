a019f81c54e93a6dcef25ace9eafb09a5379efb0
ben0429 <hadeksim0429@gmail.com>
2026-02-07 00:04:19 +0900

    feat: 로그인 페이지 정적 UI 구현

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,49 +1,39 @@
-import Image from 'next/image';
-
 const Login = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      로그인 페이지
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
-        <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={100} height={20} priority />
-        <div>test2</div>
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl leading-10 font-semibold tracking-tight text-black dark:text-zinc-50">To get started, edit the page.tsx file.</h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{' '}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>
-            or the
-            <a href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app" className="font-medium text-zinc-950 dark:text-zinc-50">
-              Learning
-            </a>
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="bg-foreground text-background flex h-12 w-full items-center justify-center gap-2 rounded-full px-5 transition-colors hover:bg-[#383838] md:w-[158px] dark:hover:bg-[#ccc]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image className="dark:invert" src="/vercel.svg" alt="Vercel logomark" width={16} height={16} />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] md:w-[158px] dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="flex h-[80vh] items-center justify-center font-sans dark:bg-black">
+      <div className="w-full max-w-[400px] rounded-xl px-6 py-8">
+        <h3 className="mb-6 text-center text-3xl font-bold">로그인</h3>
+        <form>
+          {/* radio button mentor/mentee */}
+          <div className="mt-3 mb-6 flex justify-center gap-3">
+            <label className="flex cursor-pointer items-center gap-3 rounded-full border border-gray-300 px-6 py-2 transition has-checked:border-gray-900 has-checked:bg-gray-900 has-checked:text-white">
+              <input type="radio" name="role" value="mentor" className="hidden" />
+              <span className="text-sm font-bold">멘토</span>
+            </label>
+
+            <label className="flex cursor-pointer items-center gap-3 rounded-full border border-gray-300 px-6 py-2 transition has-checked:border-gray-900 has-checked:bg-gray-900 has-checked:text-white">
+              <input type="radio" name="role" value="mentee" defaultChecked className="hidden" />
+              <span className="text-sm font-bold">멘티</span>
+            </label>
+          </div>
+
+          <div className="space-y-4">
+            <div>
+              <label className="mb-1 block text-sm font-medium text-gray-700">이메일</label>
+              <input type="text" required className="w-full rounded-lg border border-gray-300 px-3 py-2 transition outline-none focus:border-[#20315E] focus:ring-2 focus:ring-[#20315E]/40" />
+            </div>
+
+            <div>
+              <label className="mb-1 block text-sm font-medium text-gray-700">비밀번호</label>
+              <input type="password" required className="w-full rounded-lg border border-gray-300 px-3 py-2 transition outline-none focus:border-[#20315E] focus:ring-2 focus:ring-[#20315E]/40" />
+            </div>
+
+            <button type="submit" className="w-full cursor-pointer rounded-3xl bg-[#20315E] px-4 py-2 font-semibold text-white transition hover:bg-[#18264A]">
+              로그인
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
